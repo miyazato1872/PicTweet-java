ad0a1f1528c59bf53b758ee71bd88dde96b5e316
miyazato1872 <miyazato5997@gmail.com>
2023-03-10 15:31:54 +0900

    プロジェクトの作成 PicTweetの新規立ち上げ

---
+++ src/test/java/in/java/learning/pictweet/PicTweetApplicationTests.java
@@ -0,0 +1,13 @@
+package in.java.learning.pictweet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PicTweetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
